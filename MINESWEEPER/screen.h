8f43e1ab923967a0da3abcb5affa3586ca5af686
NGUYỄN MAI VĨNH <21125031@student.hcmus.edu.vn>
2021-12-06 12:54:18 +0700

    Add Windows's clear_screen()
    Add Windows implementation of clear_screen() recommended by MS:
    https://docs.microsoft.com/en-us/windows/console/clearing-the-screen
    [Not tested yet]

--- MINESWEEPER/screen.h
+++ MINESWEEPER/screen.h
@@ -4,6 +4,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 /* Inlining in different compiler
  * Checkout https://www.greenend.org.uk/rjk/tech/inline.html
  */
@@ -16,11 +19,50 @@
 /** clear_screen - Clear terminal
  */
 #ifdef __unix__
+#ifdef __GNUC__
+__attribute__((always_inline))
+#endif /* not __GNUC__ */
 INLINE void clear_screen() {
 	printf("\033c");}
 #elif defined _WIN32
-INLINE void clear_screen() {
-	system("clear");}
+/* Checkout https://docs.microsoft.com/en-us/windows/console/clearing-the-screen for more information on the following code */
+void clear_screen() {
+   HANDLE hStdOut;
+
+    hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // Fetch existing console mode so we correctly add a flag and not turn off others
+    DWORD mode = 0;
+    if (!GetConsoleMode(hStdOut, &mode))
+    {
+        return ::GetLastError();
+    }
+
+    // Hold original mode to restore on exit to be cooperative with other command-line apps.
+    const DWORD originalMode = mode;
+    mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+
+    // Try to set the mode.
+    if (!SetConsoleMode(hStdOut, mode))
+    {
+        return ::GetLastError();
+    }
+
+    // Write the sequence for clearing the display.
+    DWORD written = 0;
+    PCWSTR sequence = L"\x1b[2J";
+    if (!WriteConsoleW(hStdOut, sequence, (DWORD)wcslen(sequence), &written, NULL))
+    {
+        // If we fail, try to restore the mode on the way out.
+        SetConsoleMode(hStdOut, originalMode);
+        return ::GetLastError();
+    }
+
+    // To also clear the scroll back, emit L"\x1b[3J" as well.
+    // 2J only clears the visible window and 3J only clears the scroll back.
+
+    // Restore the mode on the way out to be nice to other command-line applications.
+    SetConsoleMode(hStdOut, originalMode);
 #endif
 
 #endif
